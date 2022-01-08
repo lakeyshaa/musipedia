b8ed484465e9865d3f7e28677165fe9a565df911
Matthew Day <matt@matthewday.net>
2022-01-08 10:54:29 -0500

    configure barebones search

--- src/Routes/Search.jsx
+++ src/Routes/Search.jsx
@@ -1,7 +1,34 @@
-const Search = () => {
-  return (
-    <p>I am SEARCH</p>
-  );
-};
+import React, { useState } from "react";
 
-export default Search;
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+  
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    // TODO connect search to Redux
+    console.log('need to hook this up');
+  }
+  return (
+      <form className="search">
+        <input
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
+    );
+}
+
+  
+
+export default Search
